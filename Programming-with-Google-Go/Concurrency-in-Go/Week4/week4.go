9b5acd65e9fcfc5ba5da77ea1bdf58f5879c754e
geekahmed <geekahmed1@gmail.com>
2020-05-02 00:22:21 +0200

    Final Commit

--- Programming-with-Google-Go/Concurrency-in-Go/Week4/week4.go
+++ Programming-with-Google-Go/Concurrency-in-Go/Week4/week4.go
@@ -1,1 +1,54 @@
-Will Be Implemented
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type ChopSticks struct {
+	sync.Mutex
+}
+type Philosopher struct {
+	lChop, rChop *ChopSticks
+	id int
+}
+var wg sync.WaitGroup
+
+func (P Philosopher) eat(){
+
+	for i := 0; i < 3 ; i++{
+		P.lChop.Lock()
+		P.rChop.Lock()
+
+		PrintPhilosoper("starting to eat", P.id)
+		time.Sleep(2 * time.Second)
+		PrintPhilosoper("finishing eating", P.id)
+		P.lChop.Unlock()
+		P.rChop.Unlock()
+		time.Sleep(2 * time.Second)
+	}
+
+	wg.Done()
+
+}
+
+func main() {
+	chops := make([]*ChopSticks, 5)
+	philosophers := make([]*Philosopher, 5)
+	for i := 0; i < 5; i++ {
+		chops[i] = new(ChopSticks)
+	}
+	for i := 0; i < 5; i++ {
+		philosophers[i] = &Philosopher{id: i, lChop: chops[i], rChop: chops[(i+1) % 5]}
+		wg.Add(1)
+		go philosophers[i].eat()
+	}
+
+	wg.Wait()
+
+}
+
+func PrintPhilosoper(name string, id int){
+	fmt.Printf("#%d is %s\n", id+1, name)
+}

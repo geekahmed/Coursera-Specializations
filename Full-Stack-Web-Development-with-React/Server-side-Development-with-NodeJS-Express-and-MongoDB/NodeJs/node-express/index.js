9d4a565da7a7924100ffd09271e813770f2e27be
geekahmed <geekahmed1@gmail.com>
2020-07-02 10:45:00 +0200

    Express Router

--- Full-Stack-Web-Development-with-React/Server-side-Development-with-NodeJS-Express-and-MongoDB/NodeJs/node-express/index.js
+++ Full-Stack-Web-Development-with-React/Server-side-Development-with-NodeJS-Express-and-MongoDB/NodeJs/node-express/index.js
@@ -1,6 +1,7 @@
 const express = require('express'), http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -8,7 +9,7 @@ const port = 3000;
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-
+app.use('/dishes', dishRouter);
 
 app.all('/dishes', (req, res, next) => {
    res.statusCode = 200;
